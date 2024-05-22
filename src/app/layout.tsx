58d6fbafdf2838dd8202434c51dad16037a2dd58
Snowi <154461763+hellosnowi@users.noreply.github.com>
2024-05-22 20:34:16 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,13 +30,13 @@ export default function RootLayout({
     if (Date().includes(date)) {
       return (
         <>
-          Today is my birthday, turning {calcAge(new Date("2004-06-28"))} years
+          Today is my birthday, turning {calcAge(new Date("2008-02-04"))} years
           old{" "}
         </>
       );
     } else
       return (
-        <>I&apos;m currently {calcAge(new Date("2004-06-28"))} years old </>
+        <>I&apos;m currently {calcAge(new Date("2008-02-04"))} years old </>
       );
   }
   return (
@@ -44,27 +44,25 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/Me.png" type="image/png" sizes="any" />
         <meta property="og:title" content="Snowi - Portfolio" />
-        <title>Snowi - Portfolio</title>
+        <title>snowi.dev</title>
         <meta name="theme-color" content="#4ca6ca" />
         <meta name="msapplication-TileColor" content="#4ca6ca" />
         <meta name="robots" content="index, follow" />
         <meta
           property="og:image"
-          content="https://forgetful.vercel.app/Me.png"
+          content="https://snowi.dev/Me.png"
         />
         <meta
           name="description"
-          content={`Hello, my name is Snowi or Sky for short. ${birthday()} and I've been coding for ${calcAge(
+          content={`Hello, my name is Snowi. ${birthday()} and I've been coding for ${calcAge(
             new Date("2019-07-03")
-          )} years. I enjoy creating open source projects on my free time or whenever I'm not lazy.`}
+          )} years. I enjoy creating projects on my free time.`}
         />
         <meta
           property="og:description"
-          content={`Hello, my name is Snowi or Sky for short. I'm currently ${calcAge(
-            new Date("2004-06-29")
-          )} years old and I've been coding for ${calcAge(
+          content={`Hello, my name is Snowi. ${birthday()} and I've been coding for ${calcAge(
             new Date("2019-07-03")
-          )} years. I enjoy creating open source projects on my free time or whenever I'm not lazy.`}
+          )} years. I enjoy creating projects on my free time.`}
         />
       </head>
       <body className={Fira.className}>
